9e02989f26d096ba59f33b8eaf336a0c45f237e2
GrazielliFonseca <graziellifonseca5@gmail.com>
2026-05-05 20:29:37 -0300

    att

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,20 @@
 import express from "express";
 
 import { AvaliacaoController } from "./controllers/avaliacaoController";
-
+import { CompraController } from "./controllers/compraController";
+import { EnderecoController } from "./controllers/enderecoController";
+import { ProdutoController } from "./controllers/produtoController";
+import { UsuarioController } from "./controllers/usuarioController";
 
 export const app = express();
 
 app.use(express.json());
 
 AvaliacaoController();
-
+CompraController();
+EnderecoController();
+ProdutoController();
+UsuarioController();
 
 app.listen(3000, () => {
   console.log("Servidor rodando em http://localhost:3000");
